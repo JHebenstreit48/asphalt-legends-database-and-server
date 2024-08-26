38841e7fa851b04a82c37fad709d6e02521acb62
Justin <j.hebenstreit25@gmail.com>
2024-08-26 11:49:52 -0500

    pushed up some current changes

---
+++ src/connection.ts
@@ -0,0 +1,25 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+import pg from 'pg';
+const { Pool } = pg;
+
+const pool = new Pool({
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  host: 'localhost',
+  database: process.env.DB_NAME,
+  port: 5432,
+});
+
+const connectToDb = async () => {
+  try {
+    await pool.connect();
+    console.log('Connected to the database.');
+  } catch (err) {
+    console.error('Error connecting to database:', err);
+    process.exit(1);
+  }
+};
+
+export { pool, connectToDb };
